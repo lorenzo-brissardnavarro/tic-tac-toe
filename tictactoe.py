e2fd6e4ec29fb38a5b434519241b88c87b04ab8c
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2025-11-19 14:10:42 +0100

    feat : fonctions grille remplie + conditions victoire

--- tictactoe.py
+++ tictactoe.py
@@ -14,6 +14,30 @@ def affichage(grille):
             print(element, end=' ')
         print()
 
+def est_remplie(grille):
+    for ligne in grille:
+        for element in ligne:
+            if element == '-':
+                return False
+    return True
+
+def victoire(grille):
+    for k in range(3):
+        if grille[k][0] != '-' and grille[k][0] == grille[k][1] == grille[k][2]:
+            print("Aligenement horizontal")
+            return True
+        if grille[0][k] != '-' and grille[0][k] == grille[1][k] == grille[2][k]:
+            print("Aligenement vertical")
+            return True
+    if grille[0][0] != '-' and grille[0][0] == grille[1][1] == grille[2][2]:
+        print("Alignement diagonale haut bas")
+        return True
+    if grille[0][2] != '-' and grille[0][2] == grille[1][1] == grille[2][0]:
+        print("Alignement diagonale bas haut")
+        return True
+    return False
+
+
 
 
 
