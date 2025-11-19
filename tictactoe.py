a91794d8c2184d7fa6b252037c80b8edd6f14492
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2025-11-19 14:58:43 +0100

    feat : fonctions complétion grille + fonctionnement jeu

--- tictactoe.py
+++ tictactoe.py
@@ -24,20 +24,46 @@ def est_remplie(grille):
 def victoire(grille):
     for k in range(3):
         if grille[k][0] != '-' and grille[k][0] == grille[k][1] == grille[k][2]:
-            print("Aligenement horizontal")
             return True
         if grille[0][k] != '-' and grille[0][k] == grille[1][k] == grille[2][k]:
-            print("Aligenement vertical")
             return True
     if grille[0][0] != '-' and grille[0][0] == grille[1][1] == grille[2][2]:
-        print("Alignement diagonale haut bas")
         return True
     if grille[0][2] != '-' and grille[0][2] == grille[1][1] == grille[2][0]:
-        print("Alignement diagonale bas haut")
         return True
     return False
 
+def changement_joueur(joueur):
+    if joueur == 'X':
+        return "O"
+    else:
+        return "X"
+        
 
+def fonctionnement():
+    grille_morpion = creation_grille()
+    joueur = random.choice(joueurs)
+    while victoire(grille_morpion) != True and est_remplie(grille_morpion) == False:
+        print(f"\033[1;34mAu tour de {joueur} de jouer !\033[0m")
+        affichage(grille_morpion)
+        horizontal = int(input("Entrez le numéro horizontal : "))-1
+        vertical = int(input("Entrez le numéro vertical : "))-1
+        while grille_morpion[horizontal][vertical] != '-':
+            horizontal = int(input("Entrez le numéro horizontal : "))-1
+            vertical = int(input("Entrez le numéro vertical : "))-1
+        grille_morpion[horizontal][vertical] = joueur
+        if victoire(grille_morpion):
+            print(f"\033[1;32mVictoire du joueur {joueur}\033[0m")
+            affichage(grille_morpion)
+            return
+        if est_remplie(grille_morpion):
+            print("\033[1;33mPartie terminée : match nul\033[0m")
+            affichage(grille_morpion)
+            return
+        joueur = changement_joueur(joueur)
 
+fonctionnement()
+        
+        
 
 
